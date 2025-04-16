24fbc2de25571bd2f2903d202426d39f8724b385
Hau Luc <richardluchau@protonmail.com>
2025-04-16 15:00:49 -0700

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("\nPlease enter your full name: ");
+        System.out.println("\nPlease enter your full name e.g. First Middle Last, Suffix: ");
         String rawInput = in.nextLine();
 
+        String suffix = "";
+        int suffixPosition = rawInput.indexOf(",");
+        if (suffixPosition != -1){
+            String[] nameNSuffix = rawInput.trim().split(",");
+            rawInput = nameNSuffix[0];
+            suffix = ", " + nameNSuffix[1].trim();
+        }
         String[] username = rawInput.trim().split(" ");
         String firstName = username[0];
 
         String middleName = "";
         if(username.length >= 3){
-            middleName = username[1];
+            middleName = " " + username[1];
         }
 
         String lastName = "";
         if (username.length > 1) {
-            lastName = username[username.length - 1];
+            lastName = " " + username[username.length - 1];
         }
 
         System.out.println("First name: " + firstName);
         System.out.println("Middle name: " + middleName);
         System.out.println("Last name: " + lastName);
-//        System.out.println("Suffix: " + suffix);
+        System.out.println("Suffix: " + suffix.replace(", ", ""));
 
-        System.out.printf("Full name: %s %s %s",firstName, middleName, lastName);
+//        System.out.printf("Full name: %s%s%s%s",firstName, middleName, lastName, suffix.length() == 0 ? "" : ", " + suffix);
+        System.out.printf("Full name: %s%s%s%s",firstName, middleName, lastName, suffix);
     }
 }
