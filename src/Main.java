a5a86a20f4df2d18c0b2ae6b7631efacaaf2c0fd
Hau Luc <richardluchau@protonmail.com>
2025-04-16 14:33:42 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("\nPlease enter your full name: ");
+        String rawInput = in.nextLine();
+
+        String[] username = rawInput.trim().split(" ");
+        String firstName = username[0];
+
+        String middleName = "";
+        if(username.length >= 3){
+            middleName = username[1];
         }
+
+        String lastName = "";
+        if (username.length > 1) {
+            lastName = username[username.length - 1];
+        }
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Last name: " + lastName);
+//        System.out.println("Suffix: " + suffix);
+
+        System.out.printf("Full name: %s %s %s",firstName, middleName, lastName);
     }
 }
